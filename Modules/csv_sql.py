c5e05e4e7e366e185cc122572f29819c6884b60c
JDavid Ram <150093295+jdavidram@users.noreply.github.com>
2025-08-10 03:28:13 -0500

    Convert CSV to SQL

--- Modules/csv_sql.py
+++ Modules/csv_sql.py
@@ -1,50 +1,65 @@
 import pandas as pd
 
+def noSymbol(txt):
+    symbols = ["(", ")"]
+    ans = txt
+    for s in symbols:
+        ans = ans.replace(s, "")
+    return ans
+
 def csv_sql(table, dirDF):
     df = pd.read_csv(dirDF)
-    sql = """DROP TABLE IF EXISTS """ + table + """;
-
-CREATE TABLE """ + table + """ (
-    ID INT PRIMARY KEY,
-"""
+    sql = "DROP TABLE IF EXISTS " + table + ";\n\n"
+    sql += """CREATE TABLE """ + table + """ (
+    ID SERIAL PRIMARY KEY"""
 
     types = ["INT", "DECIMAL", "VARCHAR", "TEXT", "DATE", "BOOLEAN"]
 
     for c in list(df):
-        sql += "    " + c + " "
-        typo = int(input(f"Seleccione el tipo de datos para { c }:\n{ "\n".join([str(n) + '-' + types[n] for n in range(0, len(types), 1)]) }\n"))
-        sql += types[typo]
+        sql += ",\n    " + noSymbol(c).replace(" ", "_") + " "
+        options = "\n".join([str(n) + '-' + types[n] for n in range(0, len(types), 1)])
+        typo = int(input(f"Seleccione el tipo de datos para '{ c }':\n{ options }\n"))
+
         if types[typo] == "DECIMAL":
-            var = [(len(str(e).replace('-','').replace('.','')), len(str(e).split('.')[1])) for e in list(df[c])]
-            sql += str(max(var))
-        elif types[typo] == "VARCHAR":
+            var = list()
+            for e in list(df[c]):
+                try:
+                    var.append((len(str(e).replace('-','').replace('.','')), len(str(e).split('.')[1])))
+                except IndexError:
+                    pass
+            sql += types[typo] + str(max(var))
+        elif types[typo] == "VARCHAR" or types[typo] == "TEXT":
             var = [len(str(u)) for u in list(df[c])]
-            sql += "(" + str((max(var)//25)*25 + 25) + ")"
-        if len([a for a in list(df[c]) if str(a) != 'nan']) == len(list(df[c])):
-            sql += " NOT NULL,\n"
+            if (max(var)//25)*25 + 25 >= 300:
+                sql += "TEXT"
+            else:
+                sql += "VARCHAR(" + str((max(var)//25)*25 + 25) + ")"
         else:
-            sql += ",\n"
+            sql += types[typo]
 
-    sql += """);
+        if len([a for a in list(df[c]) if str(a) != 'nan']) == len(list(df[c])):
+            sql += " NOT NULL"
 
-INSERT INTO """ + table + """ (""" + ", ".join(list(df)) + """) VALUES
+    sql += """\n);
+
+INSERT INTO """ + table + """ (""" + ", ".join([noSymbol(t).replace(" ", "_") for t in list(df)]) + """) VALUES
 """
 
     for i in range(0, len(df), 1):
-        row = "    (" + str(i)
-        for j in list(df.iloc[i]):
+        row = "    ('" + list(df.iloc[i])[0] + "'"
+        for j in list(df.iloc[i])[1::]:
             row += ", '" + str(j) + "'"
         if i == len(df) -1:
             row += ");"
         else:
             row += "),\n"
-        sql += row
+        sql += row.replace("nan", "")
     return sql
 
 if __name__ == '__main__':
-    table = 'gestoresRCD'
-    sql = csv_sql(table, '../DATA/GestoresRCD.csv')
+    table = 'itemsEjec'
+    sql = csv_sql(table, '../DATA/itemsEjec.csv')
     with open('../DATA/' + table + '.sql', 'w') as file:
         file.write(sql)
-    print('Se convirtió el CSV a SQL con exito!')
     print(sql)
+    print('Se convirtió el CSV a SQL con exito!')
